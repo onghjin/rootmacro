f0f41b2bbd729cc41cd5f9d4d7d544a3b0719e9f
onghjin <43430982+onghjin@users.noreply.github.com>
2018-09-20 21:57:07 +0900

    Update anasetrun.sh

--- anasetrun.sh
+++ anasetrun.sh
@@ -2,17 +2,8 @@
 #
 # Usage: ./anasetrun.sh runnumber
 #
-#   .L /home/daq/Desktop/s436/offline/jin2018jan/chkcaldata.C;
 filename='pd14Jul0'$1'_CAL.root'
 rm -f calFile.root 
 ln -s $filename calFile.root
 echo $1 > chkrun.txt
 root -b chkloop.C
-#root -b <<EOF
-#{
-#  gROOT->LoadMacro("chkcaldata.C");
-#  chkcaldata t;
-#  t.Loop();
-#}
-#EOF
-
