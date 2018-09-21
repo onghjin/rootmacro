ee0c65f288077944d145e1b8f6dd78ee3455e912
onghjin <43430982+onghjin@users.noreply.github.com>
2018-09-21 17:27:30 +0900

    Update anasetrun.sh

--- anasetrun.sh
+++ anasetrun.sh
@@ -1,9 +1,24 @@
 #! /bin/sh
 #
-# Usage: ./anasetrun.sh runnumber
+# Usage: ./anasetrun.sh runmin runmax
 #
-filename='pd14Jul0'$1'_CAL.root'
-rm -f calFile.root 
-ln -s $filename calFile.root
-echo $1 > chkrun.txt
-root -b -q chkloop.C
+runmin=$1
+runmax=$2
+runnum=$runmin
+
+echo From run$runmin to run$runmax
+while :
+do
+  if [ $runnum -le $runmax ]; then
+    echo Plot figures for run$runnum
+    filename='pd14Jul0'$runnum'_CAL.root'
+    rm -f calFile.root 
+    ln -s $filename calFile.root
+    echo $runnum > chkrun.txt
+    root -b -q chkloop.C
+    let runnum=runnum+1
+  else
+    echo End 
+    exit
+  fi
+done
